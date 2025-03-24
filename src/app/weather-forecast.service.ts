39baa9891aebe5a13cd1ba51f6a58977806369d2
piyalidas.gcetts@gmail.com <piyalidas.gcetts@gmail.com>
2025-03-24 16:17:40 +0530

    Third commit

---
+++ src/app/weather-forecast.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../environments/environment.development';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherForecastService {
+
+  constructor() { }
+  private http = inject(HttpClient);
+  private apiUrl = environment.apiURL + '/WeatherForecast'
+
+  public get(): Observable<any>{
+    debugger;
+    return this.http.get(this.apiUrl);
+  }
+}
